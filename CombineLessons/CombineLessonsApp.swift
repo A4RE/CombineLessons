c645eb6ac527a696d8f53bcf3e2e5340f3afb873
Andrei <aakoval01@gmail.com>
2024-04-05 14:30:08 +0300

    Initial Commit

---
+++ CombineLessons/CombineLessonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineLessonsApp.swift
+//  CombineLessons
+//
+//  Created by Андрей Коваленко on 05.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct CombineLessonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
